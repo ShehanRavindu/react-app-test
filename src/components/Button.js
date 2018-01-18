cf9e8fd80711b32276ebea6f7308acdce5e8e971
Shalitha Suranga <shalithasuranga@gmail.com>
2018-01-18 09:47:03 +0530

    Add react styles css

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,8 +2,16 @@ import React, { Component } from 'react';
 
 class Button extends React.Component {
     render(){
+        let buttonStyle = {
+            color : 'red',
+            border : 'none',
+            backgroundColor : '#bbbbbb',
+            padding : '6px',
+            fontSize : '20px',
+        };
+
         return (
-            <input type='button' value='Button' />
+            <input type='button' value='Button' style={buttonStyle} />
         );
     }
 }
