42aec627c14d3757a4e9b5c6fd82ba500cd59466
Shalitha Suranga <shalithasuranga@gmail.com>
2018-01-18 09:27:11 +0530

    Initial commit :rocket:

---
+++ src/components/Button.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Button extends React.Component {
+    render(){
+        return (
+            <input type='button' value='Button' />
+        );
+    }
+}
+
+
+export default Button;
